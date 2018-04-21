56d88dfafc5c5473d12b8647640f34511b9ea226
Alexandru Voica <alexandru.voica16@gmail.com>
2018-04-21 16:18:46 +0300

    Added to Checkout class

--- checkouts.js
+++ checkouts.js
@@ -1,11 +1,35 @@
 class Checkout {
 
-  construcotr (media, user, loanDate, dueDate, returnDate) {
+  constructor (media, user) {
     this._media = media;
     this._user = user;
-    this._loanDate = loanDate;
-    this._dueDate = dueDate;
-    this._returnDate = '';
+    this._loanDate = new Date();
+    this._returnDate = undefined;
   }
 
+  get media () {
+    return this._media;
+  }
+
+  get user () {
+    return this._user;
+  }
+
+  get loanDate () {
+    return this._loanDate;
+  }
+
+  get dueDate () {
+    let dueDateCalculated = this._loanDate;
+    dueDateCalculated.setDate(dueDateCalculated.getDate() + 14);
+    return dueDateCalculated;
+  }
+
+  get returnDate () {
+    return this._returnDate;
+  }
+
+  mediaHasBeenReturned () {
+    this._returnDate = new Date();
+  }
 }
