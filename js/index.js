e4fd5c1e8c494f376c3d370cde8b86db0551ab27
Alexandru Voica <alexandru.voica16@gmail.com>
2018-05-21 15:23:58 +0300

    Updated file structure of project

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
 // Short-term project:
 // Create a class system that emulates a library that holds multiple types of media
 
-import { Book, BookCopy, Movie, Article, Thesis } from './media';
+import { Book, BookCopy, Movie, Article, Thesis } from './media.js';
 // Book (title, authorsArray, pages, category)
 // BookCopy (bookObjectReference)
 // Movie (title, director, length)
 // Article (title, authors, size, publisher, keywords)
 // Thesis (title, author, size, coordinator, department, keywords)
 
-import { Librarian, Student, Visitor } from './users';
+import { Librarian, Student, Visitor } from './users.js';
 // Librarian (name, birthday)
 // Student (name, birthday, department)
 // Professor (name, birthday, department)
 // Visitor (name)
 
-import Rating from './ratings';
+import Rating from './ratings.js';
 // Rating (user, grade, comment)
 
-import Checkout from './checkouts';
+import Checkout from './checkouts.js';
 // Checkout (media, user)
   // a Checkout also has the following properties: loanDate, dueDate, returnDate
   // loanDate is set to the date the Checkout instance is created
